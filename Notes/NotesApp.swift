ea53b11029a188e5a0c14f6ba89f9efe4e48420d
Max6me <maxime.tanter@hotmail.fr>
2025-01-21 15:06:40 +0100

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Maxime Tanter on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by Maxime Tanter on 21/01/2025.
+//
+
+import Testing
+@testable import Notes
+
+struct NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
